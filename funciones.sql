492824458e8d46f86de463970ed64965c50a3537
Segundo Fariña <segundofarina@MacBook-Pro-de-Segundo.local>
2017-11-16 19:53:43 -0300

    funciones wrapper

--- funciones.sql
+++ funciones.sql
@@ -25,7 +25,9 @@ CREATE OR REPLACE FUNCTION agregarPeliculas()
 RETURNS VOID AS $$
 DECLARE
 	aFilm pelicula%ROWTYPE;
-	myCursor CURSOR FOR SELECT id_film,title,title_es,title_en,title_orig,year_,plot,synopsis_es,synopsis_en,tagline,duration,color,id_youtube,url_ticket FROM film;
+	myCursor CURSOR FOR SELECT 	id_film,title,title_es,title_en,title_orig,year_,plot,synopsis_es,
+								synopsis_en,tagline,duration,color,id_youtube,url_ticket 
+						FROM film;
 BEGIN
 	OPEN myCursor;
 	LOOP
@@ -50,73 +52,56 @@ $$ LANGUAGE plpgsql;
 
 
 --/
+CREATE OR REPLACE FUNCTION checkInsertDirector(IN id_director TEXT, IN id_film TEXT)
+RETURNS VOID AS $$
+BEGIN
+       IF(isDirector(id_director)) THEN
+                insert into dirige values(id_director,id_film);
+          END IF;
+END
+$$ LANGUAGE plpgsql;
+/
+
+CREATE OR REPLACE FUNCTION checkInsertPais(IN id_film TEXT, IN id_country TEXT)
+RETURNS VOID AS $$
+BEGIN
+       IF(isPais(id_country)) THEN
+			INSERT INTO pertenece VALUES(id_film,id_country1);
+		END IF;
+END
+$$ LANGUAGE plpgsql;
+/
+
+--/
 CREATE OR REPLACE FUNCTION agregarDirgidaPor()
 RETURNS VOID AS $$
 DECLARE
 	directores RECORD;
-	myCursor CURSOR FOR SELECT id_film,id_director1,
-		id_director2,
-		id_director3,
-		id_director4,
-		id_director5,
-		id_director6,
-		id_director7,
-		id_director8,
-		id_director9,
-		id_director10,
-		id_director11,
-		id_director12,
-		id_director13,
-		id_director14 FROM film;
+	myCursor CURSOR FOR SELECT 	id_film,id_director1,id_director2,id_director3,id_director4,
+								id_director5,id_director6,id_director7,id_director8,id_director9,
+								id_director10,id_director11,id_director12,id_director13,id_director14
+						FROM film;
 
 BEGIN
 	OPEN myCursor;
 	LOOP
 		FETCH myCursor INTO directores;
 		EXIT WHEN NOT FOUND;
-		
-		IF(isDirector(directores.id_director1)) THEN
-			INSERT INTO dirige VALUES(directores.id_director1,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director2)) THEN
-			INSERT INTO dirige VALUES(directores.id_director2,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director3)) THEN
-			INSERT INTO dirige VALUES(directores.id_director3,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director4)) THEN
-			INSERT INTO dirige VALUES(directores.id_director4,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director5)) THEN
-			INSERT INTO dirige VALUES(directores.id_director5,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director6)) THEN
-			INSERT INTO dirige VALUES(directores.id_director6,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director7)) THEN
-			INSERT INTO dirige VALUES(directores.id_director7,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director8)) THEN
-			INSERT INTO dirige VALUES(directores.id_director8,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director9)) THEN
-			INSERT INTO dirige VALUES(directores.id_director9,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director10)) THEN
-			INSERT INTO dirige VALUES(directores.id_director10,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director11)) THEN
-			INSERT INTO dirige VALUES(directores.id_director11,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director12)) THEN
-			INSERT INTO dirige VALUES(directores.id_director12,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director13)) THEN
-			INSERT INTO dirige VALUES(directores.id_director13,directores.id_film);
-		END IF;
-		IF(isDirector(directores.id_director14)) THEN
-			INSERT INTO dirige VALUES(directores.id_director14,directores.id_film);
-		END IF;     
+		PERFORM checkInsertDirector(directores.id_director1,directores.id_film);
+      	PERFORM checkInsertDirector(directores.id_director2,directores.id_film);
+     	PERFORM checkInsertDirector(directores.id_director3,directores.id_film);
+      	PERFORM checkInsertDirector(directores.id_director4,directores.id_film);
+      	PERFORM checkInsertDirector(directores.id_director5,directores.id_film);
+      	PERFORM checkInsertDirector(directores.id_director6,directores.id_film);
+      	PERFORM checkInsertDirector(directores.id_director7,directores.id_film);
+      	PERFORM checkInsertDirector(directores.id_director8,directores.id_film);
+      	PERFORM checkInsertDirector(directores.id_director9,directores.id_film);
+      	PERFORM checkInsertDirector(directores.id_director10,directores.id_film);
+      	PERFORM checkInsertDirector(directores.id_director11,directores.id_film);
+      	PERFORM checkInsertDirector(directores.id_director12,directores.id_film);
+      	PERFORM checkInsertDirector(directores.id_director13,directores.id_film);
+      	PERFORM checkInsertDirector(directores.id_director14,directores.id_film);
+
 	END LOOP;
 	CLOSE myCursor;
 END
@@ -140,15 +125,9 @@ CREATE OR REPLACE FUNCTION agregarPertenece()
 RETURNS VOID AS $$
 DECLARE
 	countries RECORD;
-	myCursor CURSOR FOR SELECT id_film,
-		id_country1,
-		id_country2,
-		id_country3,
-		id_country4,
-		id_country5,
-		id_country6,
-		id_country7,
-		id_country8 FROM film;
+	myCursor CURSOR FOR SELECT 	id_film,id_country1,id_country2,id_country3,id_country4,
+								id_country5,id_country6,id_country7,id_country8
+						FROM film;
         
 BEGIN
 	OPEN myCursor;
@@ -156,30 +135,15 @@ BEGIN
 		FETCH myCursor INTO countries;
 		EXIT WHEN NOT FOUND;
 		
-		IF(isPais(countries.id_country1)) THEN
-			INSERT INTO pertenece VALUES(countries.id_film,countries.id_country1);
-		END IF;
-		IF(isPais(countries.id_country2)) THEN
-			INSERT INTO pertenece VALUES(countries.id_film,countries.id_country2);
-		END IF;
-		IF(isPais(countries.id_country3)) THEN
-			INSERT INTO pertenece VALUES(countries.id_film,countries.id_country3);
-		END IF;
-		IF(isPais(countries.id_country4)) THEN
-			INSERT INTO pertenece VALUES(countries.id_film,countries.id_country4);
-		END IF;
-		IF(isPais(countries.id_country5)) THEN
-			INSERT INTO pertenece VALUES(countries.id_film,countries.id_country5);
-		END IF;
-		IF(isPais(countries.id_country6)) THEN
-			INSERT INTO pertenece VALUES(countries.id_film,countries.id_country6);
-		END IF;
-		IF(isPais(countries.id_country7)) THEN
-			INSERT INTO pertenece VALUES(countries.id_film,countries.id_country7);
-		END IF;
-		IF(isPais(countries.id_country8)) THEN
-			INSERT INTO pertenece VALUES(countries.id_film,countries.id_country8);
-		END IF;      
+
+		PERFORM checkInsertPais(countries.id_film,countries.id_country1);
+		PERFORM checkInsertPais(countries.id_film,countries.id_country2);
+		PERFORM checkInsertPais(countries.id_film,countries.id_country3);
+		PERFORM checkInsertPais(countries.id_film,countries.id_country4);
+		PERFORM checkInsertPais(countries.id_film,countries.id_country5);
+		PERFORM checkInsertPais(countries.id_film,countries.id_country6);
+		PERFORM checkInsertPais(countries.id_film,countries.id_country7);
+		PERFORM checkInsertPais(countries.id_film,countries.id_country8);
 	END LOOP;
 	CLOSE myCursor;
 END
